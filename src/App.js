eb75480dfbac4e28538fa2debe82a11f2ed3e8f6
kunal <kunalbankoti9@gmail.com>
2022-07-19 12:08:48 +0530

    Add Questions component to  App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import './style.css'
-import Intro from './components/Intro';
-
+import Intro from './components/Intro'
+import Questions from './components/Questions'
 function App() {
     return (
         <main>
             <img className='blob-2' src={require('./images/blob 5.png')} alt='blob' />
             <img className='blob-1' src={require('./images/blob 1.png')} alt='blob' />
             <Intro />
+            <Questions />
         </main>
     )
 }

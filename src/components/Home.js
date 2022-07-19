27746df3f564291af7fa9c811db08602302aa13a
kunal <kunalbankoti9@gmail.com>
2022-07-19 10:33:53 +0530

    Add home page

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div>
+            <h1>Quizzical</h1>
+            <p>
+                how about taking a quiz
+            </p>
+            <button>Start quizz</button>
+        </div>
+    )
+}
+export default Home;

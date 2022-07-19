fc3ccb770b9c939efa766333f780dda7c55fd2d6
kunal <kunalbankoti9@gmail.com>
2022-07-19 11:12:21 +0530

    Add style to main in App component
    main is container for every other custom component in App.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
-
+import './style.css'
 import Home from './components/Home'
 
 function App() {
     return (
-        <Home />
+        <main>
+            <img className='blob-2' src={require('./images/blob 5.png')} alt='blob' />
+            <img className='blob-1' src={require('./images/blob 1.png')} alt='blob' />
+            <Home />
+        </main>
     )
 }
 export default App;

b4241a136a5c247827053f3423d4df059e5763ff
kunal <kunalbankoti9@gmail.com>
2022-07-19 12:17:06 +0530

    Add condition to start quiz
    Add state named 'start' to App component which shows Intro-page if set false
    otherwise Questions-page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import React from 'react'
 import './style.css'
 import Intro from './components/Intro'
 import Questions from './components/Questions'
+
 function App() {
+    const [start, setStart] = React.useState(false)
+
+    function startQuiz() {
+        setStart(true)
+    }
+
     return (
         <main>
             <img className='blob-2' src={require('./images/blob 5.png')} alt='blob' />
             <img className='blob-1' src={require('./images/blob 1.png')} alt='blob' />
-            <Intro />
-            <Questions />
+            {start ? <Questions /> : <Intro />}
         </main>
     )
 }

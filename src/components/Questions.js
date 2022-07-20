cf9c1527d9de04adb9a1ae15c0db079d95fcd23f
kunal <kunalbankoti9@gmail.com>
2022-07-20 13:37:05 +0530

    Add state to Questions component to hold fetched data

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,6 +1,17 @@
-import React from 'react'
+import userEvent from '@testing-library/user-event';
+import React, { useEffect } from 'react'
 
 function Questions() {
+
+    const [data, setData] = React.useState([])
+
+    React.useEffect(() => {
+        console.log('calleffect')
+        fetch('https://opentdb.com/api.php?amount=5&category=9&type=multiple').
+            then((response) => response.json()).
+            then(result => setData(result.results))
+    }, [])
+
     return (
         <h1>
             Questions

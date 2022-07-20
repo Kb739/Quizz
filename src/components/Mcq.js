4fe37c77547125db6bdee5dd2a7884170ccc7691
kunal <kunalbankoti9@gmail.com>
2022-07-20 22:06:14 +0530

    Add feature to make answer choice
    clicking a answer button updates resepctive question's data to newly
    selected_answer , based on the match with that value changes color of
    the button.

--- src/components/Mcq.js
+++ src/components/Mcq.js
@@ -1,27 +1,42 @@
 import React, { useCallback } from 'react'
+import { nanoid } from 'nanoid'
 import he from 'he'
+
 function Mcq(props) {
 
-    const [choices, setChoices] = React.useState([])
+    const [buttons, setButtons] = React.useState([])
 
     React.useEffect(() => {
-        setChoices(arrangeChoices(props))
+        setButtons(reorderButtons(props))
     }, [props.question])
 
-    function arrangeChoices(props) {
+    function reorderButtons(props) {
         const arr = [...props.incorrect_answers]
         const randomIndex = Math.floor(Math.random() * arr.length)
         arr.splice(randomIndex, 0, props.correct_answer)
-        return arr;
+        return arr.map(answer => ({ id: nanoid(), value: answer }))
     }
 
-    const options = choices.map(answer => <button>{he.decode(answer)}</button>)
+    function buttonClick(id) {
+        const clickedButton = buttons.find(btn => btn.id === id)
+        props.onSelect(clickedButton.value)
+    }
+
+    const options = buttons.map(btn => {
+        const style = { backgroundColor: props.selected_answer === btn.value ? '#D6DBF5' : 'white' }
+        return (
+            < button style={style}
+                key={btn.id} onClick={() => buttonClick(btn.id)}> {he.decode(btn.value)}
+            </button >
+        )
+    })
+
     return (
         <div>
             <p className='question'>
                 {he.decode(props.question)}
             </p>
-            <li className='answers'>
+            <li className='choices'>
                 {options}
             </li>
 
--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,21 +1,43 @@
 import React from 'react'
 import Mcq from './Mcq'
-
+import { nanoid } from 'nanoid'
 function Questions() {
 
     const [data, setData] = React.useState([])
-
     React.useEffect(() => {
-        console.log('calleffect')
         fetch('https://opentdb.com/api.php?amount=5&category=9&type=multiple').
             then((response) => response.json()).
-            then(result => setData(result.results))
+            then(result => setData(() => {
+                const arr = result.results;
+                return arr.map(obj => ({ info: { ...obj, selected_answer: "" }, id: nanoid() }))
+            }))
     }, [])
 
-    const questions = data.map(info => <Mcq{...info} />)
+    function updateSelection(answer, id) {
+        setData((prevData) => prevData.map(qns => (qns.id === id ?
+            {
+                ...qns,
+                info:
+                {
+                    ...qns.info,
+                    selected_answer: answer
+                }
+            } : qns)
+        ))
+    }
+
+    const questions = data.map(qns => {
+        return (
+            <div key={qns.id}>
+                <Mcq {...qns.info} onSelect={(answer) => updateSelection(answer, qns.id)} />
+                <div className='line'></div>
+            </div>
+        )
+
+    })
 
     return (
-        <div>
+        <div className='quiz'>
             {questions}
         </div>
     )
